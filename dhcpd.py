373cbe79a5358fecb45e09cdbfd3d23952d8ada0
Florent LORNE <florent.lorne@univ-smb.fr>
2023-07-05 17:17:17 +0200

    First commit

---
+++ dhcpd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request, __version__
+import re
+
+
+app = Flask(__name__)
+print(f"Flask : {__version__}")
+
+
+# Pour lancer le service : flask run --host=0.0.0.0
+# Pour tester : curl -X POST http://fog-z220.local.univ-savoie.fr:5000 -H 'Content-Type: application/json' -d '{"mac_address":"00:80:C1:B2:D3:E3"}'
+@app.route("/", methods=["POST"])
+def index():
+    mac_address = request.json["mac_address"]
+    with open("/var/log/messages") as messages:
+        messages_content = messages.read()
+        messages.close()
+    ip_addr = re.search(r"DHCPACK on (.*) to " + mac_address, messages_content)
+
+    return f"{ip_addr.group(1)}"
